9dd1259bf49dc8197d03e9777fc24a587e0bfe5c
Simon Nyman <simon.nyman@ATG43787m.local>
2018-11-29 14:53:46 +0100

    Another try..

--- main.lua
+++ main.lua
@@ -43,6 +43,12 @@ function love.update(dt)
     if not love.keyboard.isDown('d') and not love.keyboard.isDown('a') then
         thePlayer.xSpeed = 0
     end
+    if love.keyboard.isDown('g') then
+        thePlayer.x = 200
+        thePlayer.y = 100
+        thePlayer.ySpeed = 0
+        thePlayer.xSpeed = 0
+    end
     
     -- Apply force to the player
     if thePlayer.airborne == 1 then
@@ -64,94 +70,65 @@ function love.update(dt)
     -- Always set the player to airborne and explicitly determine whether it should be set to 0
     thePlayer.airborne = 1
     
-    for i = tileX - 1, tileX + 1 do
-        for j = tileY - 1, tileY + 1 do
-            --print("x: "..xx..", y: "..yy)
-            if i > 0 and j > 0 and i <= 10 and j <= 10 then
-                if table[i][j] == 1 then
-                    -- Convert the tile positions into coordinates
-                    x = (i - 1) * tilesize
-                    y = (j - 1) * tilesize
-                    w = tilesize + x
-                    h = tilesize + y
-                    
-                    -- Playeris positioned based on its center point, create its bounding box
-                    px = nextX - thePlayer.width / 2
-                    py = nextY - thePlayer.height / 2
-                    pw = px + thePlayer.width
-                    ph = py + thePlayer.height
-                    
-                    -- print("x: "..x..", y: "..y.." --- w: "..w..", h: "..h.." Player px: "..px..", py: "..py.." pw: "..pw..", ph: "..ph)
-                    -- print(i..", "..j.."  "..xx..", "..yy)
-                    -- Two items are overlapping depending on direction
-                    if xx == -1 and yy == -1 then
-                        -- Upper left -> x - 1, y - 1 => stops negative x and negative y movement
-                        if px < w and py < h then
-                            --print("1")
-                            nextX = thePlayer.x
-                            nextY = thePlayer.y    
-                        end
-                    end
-                    if xx == 0 and yy == -1 then
-                        -- Upper middle -> x -1, y => stops negative y movement
-                        if py < h then
-                            --print("3")
-                            nextY = thePlayer.y
-                        end
-                    end
-                    if xx == 1 and yy == -1 then
-                        -- Upper right -> x - 1, y + 1 => stops positive z and negative y movement
-                        if pw > x and py < y then
-                            --print("4")
-                            nextX = thePlayer.x    
-                            nextY = thePlayer.y
-                        end
-                    end
-                    if xx == -1 and yy == 0 then
-                        -- Middle left -> x, y - 1 => stops negative x movement
-                        if px < w then
-                            --print("6")
-                            nextX = thePlayer.x
-                        end
-                    end
-                    if xx == 1 and yy == 0 then
-                        -- Middle right -> x, y + 1 => stops positive x movement
-                        if pw > x then
-                            --print("7")
-                            nextX = thePlayer.x
-                        end
-                    end
-                    if xx == -1 and yy == 1 then
-                        -- Lower left -> x + 1, y - 1 => stops negative x and positive y movement
-                        if pw < w and ph > y then
-                            --print("8")
-                            nextX = thePlayer.x
-                            nextY = thePlayer.y
-                        end
-                    end
-                    if xx == 0 and yy == 1 then
-                        -- Lower middle -> x + 1, y => stops positive y movement
-                        if ph > y then
-                            --print("10")
-                            nextY = thePlayer.y
-                            thePlayer.airborne = 0
-                        end
-                    end
-                    if xx == 1 and yy == 1 then
-                        -- Lower right -> x + 1, y + 1 => stops positive x and positive y movement
-                        if pw > x and ph > y then
-                            --print("11")
-                            nextX = thePlayer.x
-                            nextY = thePlayer.y
-                        end
-                    end
-                end
+    tileXCheck = tileX
+    tileYCheck = tileY
+    
+    -- Check for collision based on xSpeed and ySpeed
+    if thePlayer.ySpeed > 0 then
+        tileYCheck = tileYCheck + 1
+    end
+    if thePlayer.ySpeed < 0 then
+        tileYCheck = tileYCheck - 1 
+    end
+    if thePlayer.xSpeed < 0 then
+        tileXCheck = tileXCheck - 1
+    end
+    if thePlayer.xSpeed > 0 then
+        tileXCheck = tileXCheck + 1
+    end
+    
+    px = nextX - thePlayer.width / 2
+    py = nextY - thePlayer.height / 2
+    pw = px + thePlayer.width
+    ph = py + thePlayer.height    
+    
+    -- Vertical check
+    if tileXCheck > 0 and tileXCheck <= 10 then
+        if table[tileXCheck][tileY] == 1 then
+            x = tileXCheck - 1 * tilesize
+            y = tileY - 1 * tilesize
+            w = tilesize + x
+            h = tilesize + y
+                        
+            if (pw > x and pw < w and ph > y and ph < h) or
+               (px < w and px > x and ph > y and ph < h) or 
+               (px > x and pw < w and ph > y and ph < h) or
+               (px > x and pw < w and py > y and ph < h) then
+                -- Collision?
+                thePlayer.xSpeed = 0
+                nextX = thePlayer.x
             end
-            yy = yy + 1
-            if yy > 1 then yy = -1 end
         end
-        xx = xx + 1
-        if xx > 1 then xx = -1 end
+    end
+    
+    if tileYCheck > 0 and tileYCheck <= 10 then
+        if table[tileX][tileYCheck] == 1 then
+            x = (tileXCheck - 1) * tilesize
+            y = (tileY - 1) * tilesize
+            w = tilesize + x
+            h = tilesize + y
+            
+            print(px..", "..pw..", "..ph.."   "..x..", "..w..", "..h)
+            if (pw > x and pw < w and ph > y and ph < h) or
+               (px < w and px > x and ph > y and ph < h) or 
+               (px > x and pw < w and ph > y and ph < h) or
+               (px > x and pw < w and py > y and ph < h) then
+                -- Collision?
+                thePlayer.ySpeed = 0
+                thePlayer.airborne = 0
+                nextY = thePlayer.y
+            end
+        end
     end
     
     thePlayer.x = nextX
